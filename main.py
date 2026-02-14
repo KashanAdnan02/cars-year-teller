59b4a46728679e22a9439e503273b2520cced464
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-02-14 19:23:13 +0500

    model trained in artifacts model_train model.joblib

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ from src.cars.pipeline.data_transformation_pipeline import DataTransformationTra
 from src.cars.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 from src.cars.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
-# try:
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.initiate_data_ingestion()
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+try:
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+except Exception as e:
+        logger.exception(e)
+        raise e
 
-# try:
-#    data_validation = DataValidationTrainingPipeline()
-#    data_validation.initiate_data_validation()
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+try:
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.initiate_data_validation()
+except Exception as e:
+        logger.exception(e)
+        raise e
 
-# try:
-#    data_ingestion = DataTransformationTrainingPipeline()
-#    data_ingestion.initiate_data_transformation()
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-# try:
-#    model_trainer = ModelTrainerTrainingPipeline()
-#    model_trainer.initiate_model_training()
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+try:
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
+except Exception as e:
+        logger.exception(e)
+        raise e
+try:
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.initiate_model_training()
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 try:
    model_evaluation = ModelEvaluationTrainingPipeline()
