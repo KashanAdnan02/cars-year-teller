cc5dc535ab286c471a3c29378e7327a1544c913d
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-02-14 19:19:13 +0500

    ran 3 steps from 5

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from src.cars.pipeline.data_transformation_pipeline import DataTransformationTra
 from src.cars.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 from src.cars.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
-try:
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.initiate_data_ingestion()
-except Exception as e:
-        logger.exception(e)
-        raise e
+# try:
+#    data_ingestion = DataIngestionTrainingPipeline()
+#    data_ingestion.initiate_data_ingestion()
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
 
-try:
-   data_validation = DataValidationTrainingPipeline()
-   data_validation.initiate_data_validation()
-except Exception as e:
-        logger.exception(e)
-        raise e
+# try:
+#    data_validation = DataValidationTrainingPipeline()
+#    data_validation.initiate_data_validation()
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
 
 try:
    data_ingestion = DataTransformationTrainingPipeline()
@@ -25,16 +25,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-try:
-   model_trainer = ModelTrainerTrainingPipeline()
-   model_trainer.initiate_model_training()
-except Exception as e:
-        logger.exception(e)
-        raise e
+# try:
+#    model_trainer = ModelTrainerTrainingPipeline()
+#    model_trainer.initiate_model_training()
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
 
-try:
-   model_evaluation = ModelEvaluationTrainingPipeline()
-   model_evaluation.initiate_model_evaluation()
-except Exception as e:
-        logger.exception(e)
-        raise e
+# try:
+#    model_evaluation = ModelEvaluationTrainingPipeline()
+#    model_evaluation.initiate_model_evaluation()
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
